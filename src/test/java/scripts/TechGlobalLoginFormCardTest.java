bf98f1c542e1e93cc58daf79b8b258f0608a6eb9
Nafiye Demir <nafiyemdemir@hotmail.com>
2023-01-27 17:35:11 -0500

    added project

---
+++ src/test/java/scripts/TechGlobalLoginFormCardTest.java
@@ -0,0 +1,110 @@
+package scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.TechGlobalFrontendTestingHomePage;
+import pages.TechGlobalLoginFormPage;
+import utilities.TestData;
+
+public class TechGlobalLoginFormCardTest extends TechGlobalBase {
+    @BeforeMethod
+    public void setPage() {
+        techGlobalFrontendTestingHomePage = new TechGlobalFrontendTestingHomePage();
+        techGlobalLoginFormPage = new TechGlobalLoginFormPage();
+
+        techGlobalFrontendTestingHomePage.headerDropdown.click();
+        techGlobalFrontendTestingHomePage.headerDropdownOptions.get(0).click();
+        techGlobalFrontendTestingHomePage.clickOnCard(15);
+    }
+
+    @Test(priority = 1, description = "Validate TechGlobal Login card")
+    public void validateLoginForm() {
+
+        Assert.assertTrue(techGlobalLoginFormPage.heading.isDisplayed());
+        Assert.assertEquals(techGlobalLoginFormPage.heading.getText(), "Login Form");
+
+        Assert.assertTrue(techGlobalLoginFormPage.usernameLabel.isDisplayed());
+        Assert.assertEquals(techGlobalLoginFormPage.usernameLabel.getText(), "Please enter your username");
+        Assert.assertTrue(techGlobalLoginFormPage.usernameInputBox.isDisplayed());
+
+        Assert.assertTrue(techGlobalLoginFormPage.passwordLabel.isDisplayed());
+        Assert.assertEquals(techGlobalLoginFormPage.passwordLabel.getText(), "Please enter your password");
+        Assert.assertTrue(techGlobalLoginFormPage.passwordInputBox.isDisplayed());
+
+        Assert.assertEquals(techGlobalLoginFormPage.loginButton.getText(), "LOGIN");
+        Assert.assertTrue(techGlobalLoginFormPage.loginButton.isDisplayed());
+
+        Assert.assertEquals(techGlobalLoginFormPage.forgotPasswordLink.getText(), "Forgot Password?");
+        Assert.assertTrue(techGlobalLoginFormPage.forgotPasswordLink.isDisplayed());
+    }
+
+    @Test(priority = 2, description = "Validate TechGlobal Login card valid login")
+    public void validLoginForm() {
+
+        techGlobalLoginFormPage.usernameInputBox.sendKeys(TestData.validUsername);
+        techGlobalLoginFormPage.passwordInputBox.sendKeys(TestData.validPassword);
+
+        techGlobalLoginFormPage.loginButton.click();
+
+        Assert.assertTrue(techGlobalLoginFormPage.successLogin.isDisplayed());
+        Assert.assertTrue(techGlobalLoginFormPage.logoutButton.isDisplayed());
+    }
+    @Test(priority = 3, description = "Validate TechGlobal Login card valid login and then logout")
+    public void validateLoginAndLogoutForm() {
+
+        techGlobalLoginFormPage.usernameInputBox.sendKeys(TestData.validUsername);
+        techGlobalLoginFormPage.passwordInputBox.sendKeys(TestData.validPassword);
+
+        techGlobalLoginFormPage.loginButton.click();
+        techGlobalLoginFormPage.logoutButton.click();
+
+        Assert.assertTrue(techGlobalLoginFormPage.loginButton.isDisplayed());
+    }
+
+    @Test(priority = 4, description = "Validate TechGlobal Login card Forgot Password? Link and Reset Password page")
+    public void validateForgotPassword() {
+
+        techGlobalLoginFormPage.forgotPasswordLink.click();
+        Assert.assertTrue(techGlobalLoginFormPage.resetPasswordHeader.isDisplayed());
+        Assert.assertTrue(techGlobalLoginFormPage.resetPasswordMessage.isDisplayed());
+        Assert.assertTrue(techGlobalLoginFormPage.emailInputBox.isDisplayed());
+        Assert.assertTrue(techGlobalLoginFormPage.resetPasswordSubmitButton.isDisplayed());
+    }
+
+    @Test(priority = 5, description = "Validate TechGlobal Login card Reset Password link")
+    public void validateResetPasswordLink() {
+
+        techGlobalLoginFormPage.forgotPasswordLink.click();
+        techGlobalLoginFormPage.emailInputBox.sendKeys(TestData.emailForResetPassword);
+        techGlobalLoginFormPage.resetPasswordSubmitButton.click();
+        Assert.assertTrue(techGlobalLoginFormPage.confirmationMessage.isDisplayed());
+    }
+
+    @Test(priority = 6, description = "Validate TechGlobal Login card invalid login with wrong username")
+    public void validateInvalidUsername(){
+
+        techGlobalLoginFormPage.usernameInputBox.sendKeys(TestData.invalidUsername);
+        techGlobalLoginFormPage.passwordInputBox.sendKeys(TestData.validPassword);
+        techGlobalLoginFormPage.loginButton.click();
+        Assert.assertTrue(techGlobalLoginFormPage.errorMessageUsername.isDisplayed());
+    }
+
+    @Test(priority = 7, description = "Validate TechGlobal Login card invalid login with wrong password")
+    public void validateInvalidPassword(){
+
+        techGlobalLoginFormPage.usernameInputBox.sendKeys(TestData.validUsername);
+        techGlobalLoginFormPage.passwordInputBox.sendKeys(TestData.invalidPassword);
+        techGlobalLoginFormPage.loginButton.click();
+        Assert.assertTrue(techGlobalLoginFormPage.errorMessagePassword.isDisplayed());
+    }
+
+    @Test(priority = 8, description = "Validate TechGlobal Login card invalid login with both wrong credentials")
+    public void validateInvalidUsernameAndPassword(){
+
+        techGlobalLoginFormPage.usernameInputBox.sendKeys(TestData.invalidUsername);
+        techGlobalLoginFormPage.passwordInputBox.sendKeys(TestData.invalidPassword);
+        techGlobalLoginFormPage.loginButton.click();
+        Assert.assertTrue(techGlobalLoginFormPage.errorMessageUsername.isDisplayed());
+    }
+}
---
+++ src/test/java/utilities/TestData.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class TestData {
+    public static final String validUsername = "TechGlobal";
+    public static final String validPassword = "Test1234";
+    public static final String emailForResetPassword = "rrr@gmail.com";
+    public static final String invalidUsername = "john";
+    public static final String invalidPassword = "1234";
+}
